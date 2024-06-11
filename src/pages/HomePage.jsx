8106d10768b7b7c30b9beca0ae3d56b64254bcc9
Titus Njema <njema3@gmail.com>
2024-06-11 12:55:53 +0300

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+import Links from "../components/Links";
+
+const HomePage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Links />
+      home 
+    </div>
+  )
+}
+
+export default HomePage
