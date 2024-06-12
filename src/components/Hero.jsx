a787a5f45efad42faff7f26bcb682b2e4cd81510
Titus Njema <njema3@gmail.com>
2024-06-12 11:30:19 +0300

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
 import "./hero.scss";
+import start from "../assets/start.mp4";
 
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="video">
+        <video src={start} autoPlay loop muted />
+        <div className="text">
+          <p>Get Followers and Likes gog therehjuuewghhhsduu</p>
+          <button>Register</button>
+      </div>
+      </div>     
       
     </div>
   )
