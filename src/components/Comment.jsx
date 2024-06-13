0c3f4980aba4476cc41dc3d9cb13461fe2b26407
Titus Njema <njema3@gmail.com>
2024-06-13 10:27:38 +0300

    first commit

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,9 +3,24 @@ import "./business.scss";
 const Comment = () => {
   return (
     <div className='comment'>
-      <div className="comment1">Comment1</div>
-      <div className="comment2">Comment</div>
-      <div className="comment3">Comment</div>
+      <div className="comment1">
+        <h4>Name</h4>
+        <p>Webmaster</p>
+        <p>"I've been a user of Follow Me I Follow You ..."</p>
+        <p>Rating</p>
+      </div>
+      <div className="comment2">
+        <h4>Name</h4>
+        <p>Webmaster</p>
+        <p>"I've been a user of Follow Me I Follow You ..."</p>
+        <p>Rating</p>
+      </div>
+      <div className="comment3">
+        <h4>Name</h4>
+        <p>Webmaster</p>
+        <p>"I've been a user of Follow Me I Follow You ..."</p>
+        <p>Rating</p>
+      </div>
     </div>
   )
 }
