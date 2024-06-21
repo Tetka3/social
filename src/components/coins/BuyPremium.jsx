39cec5569c7e029dcf4f89246b9f3a0cd7f347b1
Titus Njema <njema3@gmail.com>
2024-06-21 18:43:27 +0300

    first commit

--- src/components/coins/BuyPremium.jsx
+++ src/components/coins/BuyPremium.jsx
@@ -1,9 +1,36 @@
 import "./styles.scss";
+import paypal from "../../assets/paypal.jpg"
 
 const BuyPremium = () => {
   return (
     <div className="premium">
-      Premium
+      <div className="top">
+        <h3>Monthly Subscription</h3>        
+      </div>
+      <div className="bottom">
+        <div className="paypal">
+          <img src={paypal} alt="paypal"/>
+        </div>
+        <div className="orders">
+          <select >
+            <option>PayPal</option>
+          </select>
+          <select>
+            <option>5000 Coins + Premium $5.99</option>
+            <option>10000 Coins + Premium $6.99</option>
+            <option>15000 Coins + Premium $7.99</option>
+            <option>20000 Coins + Premium $8.99</option>
+            <option>25000 Coins + Premium $9.99</option>
+            <option>30000 Coins + Premium $10.99</option>
+            <option>35000 Coins + Premium $11.99</option>            
+            <option>40000 Coins + Premium $12.99</option>            
+          </select>   
+          <button>ORDER NOW</button>
+        </div>
+        <div className="info">
+          <p>More Information</p>                    
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/coins/RedeemCoupon.jsx
+++ src/components/coins/RedeemCoupon.jsx
@@ -3,7 +3,15 @@ import "./styles.scss";
 const RedeemCoupon = () => {
   return (
     <div className='redeem'>
-      redeem 
+      <div className="top">
+        <h3>Redeem Coupon</h3>        
+      </div>
+      <div className="bottom">
+        <input type="text"/>          
+      </div>
+      <div className="info">
+        <button>REDEEM COUPON</button>                    
+      </div>      
     </div>
   )
 }
