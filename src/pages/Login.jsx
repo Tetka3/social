e4a2702b6a081ae3b28978896e1c8a57186374f8
Titus Njema <njema3@gmail.com>
2024-06-21 18:13:59 +0300

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,14 @@
 import "./home.scss";
-import { Link } from "react-router-dom";
+
 
 const Login = ({open}) => {
     if (!open) return null;
   return (
     <div className="login">
       <input type="text" placeholder="Username"/>
-      <input type="password" placeholder="Enter password"/>
+      <input type="text" placeholder="Enter password"/>
       <input type="checkbox" />
-      <button><Link to="/dashboard" >Sign in</Link></button>
+      <a href="/dashboard" className="linked">Sign in</a>
     </div>
   )
 }
