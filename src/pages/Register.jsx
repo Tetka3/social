c7ef09fc3e613b40b57a7e1f216765997443526b
Titus Njema <njema3@gmail.com>
2024-06-23 19:45:28 +0300

    new

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,9 @@
 import "./home.scss";
 import write from "../assets/write.png";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
   return (
     <div className="register">
       <div className="imaged">
@@ -15,7 +17,7 @@ const Register = () => {
         <input type="text" placeholder="Confirm Email..."/>
         <input type="checkbox" />
       </form>
-      <button>Sign Me Up!</button>
+      <button onClick={() => navigate("/dashboard")}>Sign Me Up!</button>
       <p>Forgot Password</p>
     </div>
   )
