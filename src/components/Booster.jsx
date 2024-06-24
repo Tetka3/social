6eecb7dd0037ec6c0c75469e2c7a0420efad1ecf
Titus Njema <njema3@gmail.com>
2024-06-24 19:56:00 +0300

    first commit

--- src/components/Booster.jsx
+++ src/components/Booster.jsx
@@ -1,9 +1,22 @@
 import "./business.scss";
+import fb from "../assets/fb.png";
 
 const Booster = () => {
   return (
     <div className='booster'>
-      booster 
+      <div className="info">
+          <h3>Social Media & Traffic Booster</h3>
+          <p>Whether you're a new business or an old one, you can count on our platforms to boost traffic. It does not matter. All that matters is your committment and desire toexcel by connecting to potential customers and business partners. You can improve your social network with the following social media platforms.</p><br></br>
+      </div>
+      <div className="socials">
+        <img src={fb}/>
+        <img src={fb}/>
+        <img src={fb}/>
+        <img src={fb}/>
+        <img src={fb}/>
+        <img src={fb}/>
+        <img src={fb}/>
+      </div>
     </div>
   )
 }
--- src/components/Follow.jsx
+++ src/components/Follow.jsx
@@ -3,8 +3,8 @@ import "./business.scss";
 const Follow = () => {
   return (
     <div className='follow'>
-      <h3>What is FOLLOW ME I FOLLOW YOU?</h3> 
-      <p>FOLLOW ME I FOLLOW YOU is a social networking site for businesses and individuals</p>
+      <h3>What is FOLLOW ME I FOLLOW YOU?</h3><br></br> 
+      <p>FOLLOW ME I FOLLOW YOU is a social networking site for businesses and individuals. The social network uses social media platforms and SEO tools to boost your search online. You can promote anything ranging from social media platforms to websites. The social network platform provides a platform for connecting with potential friends, business partners and customers.</p><br></br>
     </div>
   )
 }
