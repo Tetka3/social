932f5d6109bfa6d29bf415b157de8c74fa3a401b
Titus Njema <njema3@gmail.com>
2024-06-24 20:04:20 +0300

    first commit

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -3,7 +3,9 @@ import "./business.scss";
 const Bookmark = () => {
   return (
     <div className='bookmark'>
-      bookmark 
+      <h3>Likes, Shares & Bookmarks</h3><br></br> 
+      <p>Why do you need likes and shares? Social media currently plays a major role in boosting your SEO. When you increase your social network, yousimply increase your social presence. You can rely on us for likes and shares. </p><br></br>
+      <button>Sign Up Free</button>
     </div>
   )
 }
