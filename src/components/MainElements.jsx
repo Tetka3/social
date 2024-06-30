3229e415749152cff569706e556c0131eea57994
Titus Njema <njema3@gmail.com>
2024-06-30 19:52:17 +0300

    first commit

--- src/components/MainElements.jsx
+++ src/components/MainElements.jsx
@@ -1,10 +1,12 @@
 import Coins from "./Coins";
 import "./business.scss";
+import MyExchanges from "./cards/MyExchanges";
 
 const MainElements = () => {
   return (
     <div className='mainElements'> 
       <Coins/>
+      <MyExchanges/>
     </div>
   )
 }
--- src/components/cards/MyExchanges.jsx
+++ src/components/cards/MyExchanges.jsx
@@ -2,7 +2,20 @@ import "./cards.scss";
 
 const MyExchanges = () => {
   return (
-    <div>
+    <div className="exchanges">
+        <div className="top">
+            <div className="top1">
+                <p>Your Custom Exchanges</p>
+            </div>
+            <div className="btn1">
+                <button>Add Custom Exchanges</button>
+                <p>Approvals Actions will appear here if there issomething toApprove or Deny</p>
+            </div>
+            <div className="top2">
+                <p>Your Links & Social Newtork Accounts</p>
+            </div>
+        </div>
+        <div className="bottom"></div>
       
     </div>
   )
