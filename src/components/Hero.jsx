727db27fc654065cb75b17642dea3d72a5d89b5c
Titus Njema <njema3@gmail.com>
2024-07-04 08:48:25 +0300

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,20 @@
 import "./hero.scss";
 import start from "../assets/start.mp4";
+import {  useNavigate } from 'react-router-dom';
 
 const Hero = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="hero">
       <div className="video">
         <video src={start} autoPlay loop muted />
         <div className="text">
           <p>Get Followers and Likes</p>
-          <button>Register</button>
+          <button onClick={() => navigate('/register')}>Register</button>
       </div>
-      </div>     
-      
+      </div> 
     </div>
   )
 }
