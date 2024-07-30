58a64b72aad20c477b47c5a0fa88efa6812064eb
Titus Njema <njema3@gmail.com>
2024-07-30 18:35:17 +0300

    first commit

--- src/components/Booster.jsx
+++ src/components/Booster.jsx
@@ -1,5 +1,9 @@
 import "./business.scss";
 import fb from "../assets/fb.png";
+import vk from "../assets/vk.jpg";
+import tumblr from "../assets/tumblr.png";
+import diigo from "../assets/diigo.jpg";
+import reddit from "../assets/reddit.png";
 import linkedin from "../assets/linkedin.jpg";
 import instagram from "../assets/instagram.jpg";
 import twitter from "../assets/twitter.jpg";
@@ -17,13 +21,17 @@ const Booster = () => {
       </div>
       <div className="socials">
         <img src={fb}/>
-        <img src={linkedin}/>
-        <img src={instagram}/>
+        <img src={linkedin}/>        
         <img src={twitter}/>
         <img src={youtube}/>
         <img src={pinterest}/>
-        <img src={snapchat}/>
-        <img src={telegram}/>      
+        <img src={snapchat}/>             
+        <img src={vk}/>      
+        <img src={reddit}/>      
+        <img src={diigo}/>      
+        <img src={tumblr}/>
+        <img src={telegram}/> 
+        <img src={instagram}/>      
       </div>
     </div>
   )
