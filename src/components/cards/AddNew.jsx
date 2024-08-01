076932555acc388935ed34551da3755efeb34784
Titus Njema <njema3@gmail.com>
2024-08-01 10:25:55 +0300

    first commit

--- src/components/cards/AddNew.jsx
+++ src/components/cards/AddNew.jsx
@@ -5,24 +5,22 @@ const AddNew = () => {
     <div className='addNew'>        
         <div className="addSocial">
             <p>Add New Site or Social Network Account</p>
-        </div>
-        <div className="addForm">
-            <form className="dataInput">
-                <p>Can't find the one you want? Add a Custom Exchange</p>
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <button>Submit Link</button>
-                <p>*Please note: The higher your CPC the faster you get Clicks, Likes, Shares, Views etc</p>
-            </form>
-        </div>            
+        </div>        
+        <form className="addForm">
+            <p>Can't find the one you want? Add a Custom Exchange</p>
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <button>Submit Link</button>
+            <p>*Please note: The higher your CPC the faster you get Clicks, Likes, Shares, Views etc</p>
+        </form>                 
     </div>    
   )
 }
