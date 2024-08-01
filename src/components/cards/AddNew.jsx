b2031fc912f1ecc597e9e45048d18dafea141be2
Titus Njema <njema3@gmail.com>
2024-08-01 10:42:32 +0300

    first commit

--- src/components/cards/AddNew.jsx
+++ src/components/cards/AddNew.jsx
@@ -8,16 +8,33 @@ const AddNew = () => {
         </div>        
         <form className="addForm">
             <p>Can't find the one you want? Add a Custom Exchange</p>
-            <input />
-            <input />
-            <input />
-            <input />
-            <input />
-            <input />
-            <input />
-            <input />
-            <input />
-            <input />
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>
+            <div className="dataInput">
+                <span>Campaign Type</span><input />
+            </div>            
             <button>Submit Link</button>
             <p>*Please note: The higher your CPC the faster you get Clicks, Likes, Shares, Views etc</p>
         </form>                 
