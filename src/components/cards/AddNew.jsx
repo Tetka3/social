80d7da3d7c61acfba0c55f9b8891e598a241a7e8
Titus Njema <njema3@gmail.com>
2024-08-01 10:23:45 +0300

    first commit

--- src/components/cards/AddNew.jsx
+++ src/components/cards/AddNew.jsx
@@ -2,29 +2,28 @@ import "./cards.scss";
 
 const AddNew = () => {
   return (
-    <div className='addNew'>
-        <div className="top">
-            <div className="top1">
-                <p>Add New Site or Social Network Account</p>
-            </div>
-            <div className="btn1">
-                <form className="dataInput">
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <input />
-                    <button>Submit Link</button>
-                    <p>*Please note: The higher your CPC the faster you get Clicks, Likes, Shares, Views etc</p>
-                </form>
-            </div>            
+    <div className='addNew'>        
+        <div className="addSocial">
+            <p>Add New Site or Social Network Account</p>
         </div>
-    </div>
+        <div className="addForm">
+            <form className="dataInput">
+                <p>Can't find the one you want? Add a Custom Exchange</p>
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <input />
+                <button>Submit Link</button>
+                <p>*Please note: The higher your CPC the faster you get Clicks, Likes, Shares, Views etc</p>
+            </form>
+        </div>            
+    </div>    
   )
 }
 
