0cdffe86e6796750b75b76ca7ac7efbaf41a7053
Titus Njema <njema3@gmail.com>
2024-08-02 07:43:14 +0300

    first commit

--- src/components/cards/MyExchanges.jsx
+++ src/components/cards/MyExchanges.jsx
@@ -14,7 +14,12 @@ const MyExchanges = () => {
             <p>Your Links & Social Network Accounts</p>
         </div>        
         <div className="addSocial">
-            <p>Your Links & Social Network Accounts</p>
+            <p>Links that are not added correctly, broken or in wrong sections will be deleted. Breaking TOS they will be banned <br></br>
+            Daily Statistics Reset in 18 hour 26 minutes</p>
+            <button></button><span>Enabled</span>
+            <button></button><span>Edit Link</span>
+            <button></button><span>Boost CPC</span>
+            <button></button><span>Delete</span>            
         </div>        
         <div className="addSocial">
             <p>Your Links & Social Network Accounts</p>
