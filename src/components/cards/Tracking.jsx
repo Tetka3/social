0cf76d508e96f35bd3de769ab50c6091ab173286
Titus Njema <njema3@gmail.com>
2024-08-05 08:45:55 +0300

    first commit

--- src/components/cards/Tracking.jsx
+++ src/components/cards/Tracking.jsx
@@ -4,10 +4,17 @@ const Tracking = () => {
   return (
     <div className='tracking'>        
         <div className="track-header">
-            <p>Your Custom Exchanges</p>
+            <p>History: See who has been clicking your links</p>
         </div> 
-        <div className="trackk-input">
-            <button>Add Custom Exchanges</button>
+        <div className="track-input">
+            <select>
+                <option>London</option>
+                <option>Berlin</option>
+                <option>Greenland</option>
+                <option>Tokyo</option>
+                <option>Paris</option>
+            </select>
+            <button>Change</button>
             <p>Approval Actions will appear here if there is something to Approve or Deny</p>
         </div>
         <div className="track-info">
