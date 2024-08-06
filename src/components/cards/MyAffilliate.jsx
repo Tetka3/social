5c8c9a0d40e4f010ecacf308e9573a2dab06569d
Titus Njema <njema3@gmail.com>
2024-08-06 08:24:23 +0300

    first commit

--- src/components/cards/MyAffilliate.jsx
+++ src/components/cards/MyAffilliate.jsx
@@ -1,37 +1,75 @@
 import React from 'react'
 
 const MyAffilliate = () => {
-  return (
-    <div className='affiliate-links'>
-        <div className="referral-statistics">
-            <div className="top-menu">
-                <p>Referral Stats, Links, Banners and Splash Pages</p>
+    return (
+        <div className='affiliate-links'>
+            <div className="referral-statistics">
+                <div className="top-menu">
+                    <p>Referral Stats, Links, Banners and Splash Pages</p>
+                </div>
+                <div className="bottom-me">
+                    <ul>
+                        <li>800 followlike Coins for each new active referral.</li>
+                        <li>15% of all your referral's Coin (credit) earnings for life.</li>
+                        <li>$0.15 Cash for each active referral. Cash out available at $20.00 via Paypal.</li>
+                        <li>Earn 1 followlike Coin for every UNIQUE visitor you send to your referral link</li>
+                    </ul>
+                    <button>Buy Referrals</button>
+                    <button>Buy Referrals</button>
+                    <button>Buy Referrals</button>
+                    <input type='text' />
+                    <input type='text' />
+                    <input type='text' />
+                    <input type='text' />
+                </div>
             </div>
-            <div className="bottom-me">
-                <ul>
-                    <li>800 followlike Coins for each new active referral.</li>
-                    <li>15% of all your referral's Coin (credit) earnings for life.</li>
-                    <li>$0.15 Cash for each active referral. Cash out available at $20.00 via Paypal.</li>
-                    <li>Earn 1 followlike Coin for every UNIQUE visitor you send to your referral link</li>
-                </ul>
-                <button>Buy Referrals</button>
-                <button>Buy Referrals</button>
-                <button>Buy Referrals</button>
-                <input type='text'/>
-                <input type='text'/>
-                <input type='text'/>
-                <input type='text'/>
+            <div className="your-links">
+                <div className="share-button">
+                    <div className="top-menu">
+                        <p>Referral Stats, Links, Banners and Splash Pages</p>
+                    </div>
+                    <div className="bottom-me">
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                    </div>
+                </div>
+                <div className="unique-link">
+                    <div className="top-menu">
+                        <p>Referral Stats, Links, Banners and Splash Pages</p>
+                    </div>
+                    <div className="bottom-me">
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                    </div>
+                </div>
+                <div className="unique-pages">
+                    <div className="top-menu">
+                        <p>Referral Stats, Links, Banners and Splash Pages</p>
+                    </div>
+                    <div className="bottom-me">
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                    </div>
+                </div>
             </div>
+            <div className="your-codes">
+                <div className="referral-statistics">
+                    <div className="top-menu">
+                        <p>Referral Stats, Links, Banners and Splash Pages</p>
+                    </div>
+                    <div className="bottom-me">
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <div className="your-links">
-            <div className="share-button">L</div>
-            <div className="unique-link">C</div>
-            <div className="unique-pages">R</div>
-        </div>
-        <div className="your-codes"></div>
-      
-    </div>
-  )
+    )
 }
 
 export default MyAffilliate
