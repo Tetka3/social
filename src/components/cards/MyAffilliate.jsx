8acade616088bb61ee797ea708900ec067b6127a
Titus Njema <njema3@gmail.com>
2024-08-07 15:30:41 +0300

    first commit

--- src/components/cards/MyAffilliate.jsx
+++ src/components/cards/MyAffilliate.jsx
@@ -14,37 +14,40 @@ const MyAffilliate = () => {
                         <li>$0.15 Cash for each active referral. Cash out available at $20.00 via Paypal.</li>
                         <li>Earn 1 followlike Coin for every UNIQUE visitor you send to your referral link</li>
                     </ul>
-                    <button>Buy Referrals</button>
-                    <button>Buy Referrals</button>
-                    <button>Buy Referrals</button>
-                    <input type='text' />
-                    <input type='text' />
-                    <input type='text' />
-                    <input type='text' />
+                    <div className="btns">
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                        <button>Buy Referrals</button>
+                    </div>
+                    <div className="input-links">
+                        <input type='text' />
+                        <input type='text' />
+                        <input type='text' />
+                        <input type='text' />
+                    </div>
                 </div>
             </div>
-            <div className="two">
             <div className="your-links">
                 <div className="top-share">
                     <p>Referral Stats, Links, Banners and Splash Pages</p>
                 </div>
                 <div className="bottom-menu">
-                    <button>Buy Referrals</button>                    
+                    <button>Buy Referrals</button>
                 </div>
                 <div className="unique-link">
                     <div className="top-menu">
                         <p>Referral Stats, Links, Banners and Splash Pages</p>
                     </div>
                     <div className="bottom-me">
-                        <button>Buy Referrals</button>                        
+                        <button>Buy Referrals</button>
                     </div>
                 </div>
-                <div className="unique-pages">
+                <div className="unique-banners">
                     <div className="top-menu">
                         <p>Referral Stats, Links, Banners and Splash Pages</p>
                     </div>
                     <div className="bottom-me">
-                        <button>Buy Referrals</button>                        
+                        <button>Buy Referrals</button>
                     </div>
                 </div>
             </div>
@@ -54,11 +57,10 @@ const MyAffilliate = () => {
                         <p>Referral Stats, Links, Banners and Splash Pages</p>
                     </div>
                     <div className="bottom-me">
-                        <button>Buy Referrals</button>                        
+                        <button>Buy Referrals</button>
                     </div>
                 </div>
             </div>
-            </div>            
         </div>
     )
 }
