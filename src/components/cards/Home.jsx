6083421d2366e1aee9b31e7937f77224bc52987e
Titus Njema <njema3@gmail.com>
2024-08-12 09:56:29 +0300

    first commit

--- src/components/cards/Home.jsx
+++ src/components/cards/Home.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="dashboard-home">
-      <div className="card-input"></div> 
-      <div className="member-actions"></div>
+      <div className="card-input">Video</div> 
+      <div className="member-actions">
+        <div className="current-actions"></div>
+        <div className="registered-actions"></div>
+      </div>
       <div className="adverts">Advertise here...</div>
     </div>
   )
