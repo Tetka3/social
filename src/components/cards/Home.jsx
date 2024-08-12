22d9875cdf7a3533f5bbf8e83f9d31f62f23b97e
Titus Njema <njema3@gmail.com>
2024-08-12 10:00:31 +0300

    first commit

--- src/components/cards/Home.jsx
+++ src/components/cards/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="dashboard-home">
-      <div className="card-input">Video</div> 
+      <div className="card-input">Video</div>
       <div className="member-actions">
         <div className="current-actions">
           <div className="topic">
@@ -15,7 +15,16 @@ const Home = () => {
             <p>Websites viewed</p>
           </div>
         </div>
-        <div className="registered-actions"></div>
+        <div className="registered-actions">
+          <div className="topic">
+            <p>Current Member Actions (Live)</p>
+          </div>
+          <div className="detailed-info">
+            <p>Websites viewed</p>
+            <p>Websites viewed</p>
+            <p>Websites viewed</p>
+          </div>
+        </div>
       </div>
       <div className="adverts">Advertise here...</div>
     </div>
