ca6a36736ea35dbd5f37c8c8d26efd3238cc5a6c
Titus Njema <njema3@gmail.com>
2024-08-12 09:58:25 +0300

    first commit

--- src/components/cards/Home.jsx
+++ src/components/cards/Home.jsx
@@ -5,7 +5,16 @@ const Home = () => {
     <div className="dashboard-home">
       <div className="card-input">Video</div> 
       <div className="member-actions">
-        <div className="current-actions"></div>
+        <div className="current-actions">
+          <div className="topic">
+            <p>Current Member Actions (Live)</p>
+          </div>
+          <div className="detailed-info">
+            <p>Websites viewed</p>
+            <p>Websites viewed</p>
+            <p>Websites viewed</p>
+          </div>
+        </div>
         <div className="registered-actions"></div>
       </div>
       <div className="adverts">Advertise here...</div>
