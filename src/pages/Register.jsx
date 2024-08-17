6784ec20291303a7450b5a80208eb2aa7c47bb6b
Titus Njema <njema3@gmail.com>
2024-08-17 09:32:52 +0300

    first commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,9 +8,14 @@ const Register = () => {
   const [registerData, setRegisterData] = useState({
     username: "",
     password: "",
-    email: ""
+    email: "",
+    repeatPassword: ""
   })
   const navigate = useNavigate();
+
+  const handleChange = () => {
+    
+  }
   return (
     <div className="register">
       <div className="imaged">
@@ -21,23 +26,29 @@ const Register = () => {
           type="text"
           placeholder="Username..."
           name = "username"
+          onChange={handleChange}
         />       
         <input
           type="text"
           placeholder="Email..." 
           name = "email"
+          onChange={handleChange}
         />        
          <input
           type="text"
           placeholder="Password..." 
           name = "password"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Repeat Password..." 
+          name ="repeatPassword"
+          onChange={handleChange}
         />
         <input
           type="checkbox" 
+          onChange={handleChange}
         />
       </form>
       <button onClick={() => navigate("/dashboard")}>Sign Me Up!</button>
