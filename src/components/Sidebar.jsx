6c96ca2808fca1c04c591a45a3f21a1f43f6d4cd
Titus Njema <njema3@gmail.com>
2024-08-19 09:24:39 +0300

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Bonus from "./Bonus";
 import "./business.scss";
 import Buttons from './Buttons';
 import EarnCoins from "./EarnCoins";
 import Updates from "./Updates";
+import Menu from "./Menu";
 import YourAccount from "./YourAccount";
 
 const Sidebar = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className='sidebar'>
       <Buttons/>
       <EarnCoins/>
+      <Menu /> 
       <YourAccount/>
       <Bonus/>
       <Updates/>
