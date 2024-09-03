11fdd96e9685014677256fcd027218ba5eaea7a3
Titus Njema <njema3@gmail.com>
2024-09-03 19:20:25 +0300

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ import YourAccount from "./YourAccount";
 
 const Sidebar = () => {
 
-  const [showMenu, setShowMenu] = useState(true);   
+  const [showMenu, setShowMenu] = useState(false);   
   
   return (
     <div className='sidebar'>
