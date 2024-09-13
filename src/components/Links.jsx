6c87b3671c327de63f7620e669b73ad5df1ed7e9
Titus Njema <njema3@gmail.com>
2024-09-13 14:44:48 +0300

    first commit

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import "./links.css"
+import { Link } from 'react-router-dom'
 
 
 const Links = () => {
   return (
     <div className='links'>
         <div className="left">            
-            <li className='activeLink'>Home</li>
-            <li>Add New</li>
-            <li>Earn Coins</li>
-            <li>Buy Coins</li>            
+            <Link to="/dashboard"><li className='activeLink'>Home</li></Link>
+            <Link to="/dashboard"><li>Add New</li></Link>
+            <Link to="/dashboard"><li>Earn Coins</li></Link>
+            <Link to="/dashboard"><li>Buy Coins</li></Link>            
         </div>
         <div className="center"></div>
         <div className="right">
             <ul>
-                <li className='activeLink'>Extras</li>
-                <li>Support</li>
-                <li>Account</li>
+                <Link to="/dashboard"><li className='activeLink'>Extras</li></Link>
+                <Link to="/dashboard"><li>Support</li></Link>
+                <Link to="/dashboard"><li>Account</li></Link>
             </ul>
         </div>
       
