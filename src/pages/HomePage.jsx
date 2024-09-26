11ce98e823ca6adb8c90bd57909f792307e3983a
Titus Njema <njema3@gmail.com>
2024-09-26 19:06:56 +0300

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
 import Hero from '../components/Hero';
-import Social from '../components/Social';
-import Website from '../components/Website';
-import Video from '../components/Video';
-import Business from '../components/Business';
+import Social from '../components/mainEl/Social';
+import Website from '../components/mainEl/Website';
+import Video from '../components/mainEl/Video';
+import Business from '../components/mainEl/Business';
 import Follow from '../components/Follow';
-import Booster from '../components/Booster';
+import Booster from '../components/mainEl/Booster';
 import Bookmark from '../components/Bookmark';
 import "./home.scss";
 import System from "../components/System";
 import Comments from "../components/Comments";
 import NavbarHome from '../components/NavbarHome';
-// import Footer from '../components/Footer';
+import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
