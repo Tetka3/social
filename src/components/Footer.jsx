67a48105af22cdba6146cb20cfa6e977c15661e7
Titus Njema <njema3@gmail.com>
2024-10-14 10:15:44 +0300

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import "./style.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="top">
+      <div className="top-footer">
         <p><b>FOLLOW ME I FOLLOW YOU</b> is neither affiliated with nor endorsed by Reddit, YouTube, Instagram, Diigo, Delicious,  Tumblr, Linkedin, VK, Twitter, Facebook, Pinterest, or any other social network or Bookmarking site</p>
       </div>
-      <div className="bottom">
+      <div className="bottom-footer">
         <ul>
           <li>FAQ</li>
           <li>Privacy</li>
