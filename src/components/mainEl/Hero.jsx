bbb87378475603369a2e25da2f146ce5b1099906
Titus Njema <njema3@gmail.com>
2024-10-14 09:19:30 +0300

    first commit

--- src/components/mainEl/Hero.jsx
+++ src/components/mainEl/Hero.jsx
@@ -8,15 +8,13 @@ const Hero = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="hero">
-      <div className="video">
-        <video src={start} autoPlay loop muted />
-        {/* <video src={likes} autoPlay loop muted /> */}
-        <div className="text">
-          <p>Get Followers and Likes</p>
-          <button onClick={() => navigate('/register')}>Register</button>
-      </div>
-      </div> 
+    <div className="hero">      
+      <video src={start} autoPlay loop muted />
+      {/* <video src={likes} autoPlay loop muted /> */}
+      <div className="text">
+        <p>Get Followers and Likes</p>
+        <button onClick={() => navigate('/register')}>Register</button>
+      </div>      
     </div>
   )
 }
